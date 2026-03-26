95e4e9cb3bfb80ae26d59861973654e2da8de4ec
giahu <giahuy.823948@gmail.com>
2026-03-26 10:42:50 +0700

    #1-add-components-HuyGia

---
+++ src/app/components/students/student-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { StudentListComponent } from './student-list/student-list.component';
+import { StudentAddComponent } from './student-add/student-add.component';
+import { StudentEditComponent } from './student-edit/student-edit.component';
+
+const routes: Routes = [
+{path: '', component: StudentListComponent },
+{path: 'student-list',component: StudentListComponent}, 
+{path: 'student-add', component: StudentAddComponent},
+{path: 'student-edit/:id',component: StudentEditComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class StudentRoutingModule {
+  
+ }
